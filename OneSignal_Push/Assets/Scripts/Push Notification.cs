dc80138b659f8a9032d1f890f91455c06833d7b4
HemnathCrion <hemnathcrion@gmail.com>
2025-09-03 19:52:57 +0530

    [Added] Popup Screen and Basic UI

--- OneSignal_Push/Assets/Scripts/Push Notification.cs
+++ OneSignal_Push/Assets/Scripts/Push Notification.cs
@@ -1,24 +1,91 @@
+using System;
 using UnityEngine;
 using OneSignalSDK;
-using OneSignalSDK.Debug.Models;
+using OneSignalSDK.Notifications;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
 
 public class PushNotification : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI statusText;
+    [SerializeField] private TextMeshProUGUI m_statusText;
+    [SerializeField] private TextMeshProUGUI m_popupText;
 
-    async void Start()
+    [SerializeField] private GameObject m_poupPanel;
+    [SerializeField] private GameObject m_homePanel;
+    [SerializeField] private Button m_popupButton;
+    [SerializeField] private Button m_backButton;
+    
+
+    private void Awake()
     {
-        // Enable verbose logging for debugging (remove in production)
-        statusText.text = "Starting...";
-        OneSignal.Debug.LogLevel = LogLevel.Verbose;
-        // Initialize with your OneSignal App ID
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private async void Start()
+    {
+        m_poupPanel.SetActive(false);
+        m_statusText.text = "Starting...";
+        OneSignal.Debug.LogLevel = OneSignalSDK.Debug.Models.LogLevel.Verbose;
+
+        // Initialize OneSignal
         OneSignal.Initialize("3eb4f1e9-37a9-460b-9e02-923205fb3ae1");
-        statusText.text = "Ready...";
-        // Use this method to prompt for push notifications.
-        // We recommend removing this method after testing and instead use In-App Messages to prompt for notification permission.
+        m_statusText.text = "Ready...";
+
+        // Ask for push permission (iOS only, Android auto-grants)
         await OneSignal.Notifications.RequestPermissionAsync(true);
-        statusText.text = "Permission granted";
-        Debug.Log(LogLevel.Verbose);
+        m_statusText.text = "Permission granted";
+
+        OneSignal.Notifications.Clicked += OnNotificationClicked;
+    }
+
+
+    private void OnDisable()
+    {
+        m_poupPanel.SetActive(false);
+        m_statusText.enabled = false;
+        m_homePanel.SetActive(false);
+    }
+
+    private void OnNotificationClicked(object sender, NotificationClickEventArgs e)
+    {
+        if (e.Notification.AdditionalData.ContainsKey("MainScene"))
+        {
+            string sceneName = e.Notification.AdditionalData["MainScene"].ToString();
+            Debug.Log("Opening scene: " + sceneName);
+            m_statusText.text = "Opening scene: " + sceneName;
+
+            if (Application.CanStreamedLevelBeLoaded(sceneName) && sceneName == "Log")
+            {
+                m_poupPanel.SetActive(true);
+                m_popupText.text = "OK Click to open your log list and review the latest system records.";
+
+                m_popupButton.onClick.AddListener(() =>
+                {
+                    OnDisable();
+                    SceneManager.LoadScene(sceneName);
+                });
+                m_backButton.onClick.AddListener(() =>
+                {
+                    SceneManager.LoadScene("MainScene");
+                });
+            }
+            else
+            {
+                m_poupPanel.SetActive(true);
+                m_popupText.text = "OK Click to open your work order list and check the latest updates.";
+
+                m_popupButton.onClick.AddListener(() =>
+                {
+                    OnDisable();
+                    SceneManager.LoadScene(sceneName);
+                });
+                m_backButton.onClick.AddListener(() =>
+                {
+                    SceneManager.LoadScene("MainScene");
+                });
+            }
         }
+    }
 }
